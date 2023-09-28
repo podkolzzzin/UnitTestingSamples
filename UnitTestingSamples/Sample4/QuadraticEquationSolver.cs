49b4436bff2bb98bc1e14efb3580078828f47c03
Andrii Podkolzin <andrii.podkolzin@namecheap.com>
2023-09-28 10:17:22 +0300

    fix add more samples

---
+++ UnitTestingSamples/Sample4/QuadraticEquationSolver.cs
@@ -0,0 +1,83 @@
+﻿namespace UnitTestingSamples.Sample4;
+
+public class QuadraticEquationSolver
+{
+  /// <summary>
+  /// Returns empty array if there are no roots.
+  /// Returns one-element array if there is one root.
+  /// Returns two-element array if there are two roots.
+  /// </summary>
+  /// <param name="a"></param>
+  /// <param name="b"></param>
+  /// <param name="c"></param>
+  /// <returns></returns>
+  /// <exception cref="NotImplementedException"></exception>
+  public static double[] Solve(double a, double b, double c)
+  {
+    var D = b * b - 4 * a * c;
+    if (D < 0)
+      return Array.Empty<double>();
+    else if (D == 0)
+      return new[] { -b / (2 * a) };
+    else
+    {
+      var x1 = (-b + Math.Sqrt(D)) / (2 * a);
+      var x2 = (-b - Math.Sqrt(D)) / (2 * a);
+      return new[] { x1, x2 };
+    }
+  }
+
+  public class QuadraticEquationSolverTests
+  {
+    [Fact]
+    public void Should_ReturnEmptyArray_When_DiscriminantIsNegative()
+    {
+      // Arrange
+      var a = 1;
+      var b = 2;
+      var c = 3;
+
+      // Act
+      var result = QuadraticEquationSolver.Solve(a, b, c);
+
+      // Assert
+      Assert.Empty(result);
+    }
+
+    [Fact]
+    public void Should_ReturnOneElementArray_When_DiscriminantIsZero()
+    {
+      // Arrange
+      var a = 1;
+      var b = 2;
+      var c = 1;
+
+
+      // Act
+      var result = QuadraticEquationSolver.Solve(a, b, c);
+
+      // Assert
+      Assert.Single(result);
+      Assert.Equal(-1, result[0]);
+    }
+
+    [Fact]
+    public void Should_ReturnTwoElementArray_When_DiscriminantIsPositive()
+    {
+      // Arrange
+      var a = 1;
+      var b = 5;
+      var c = 1;
+
+      // D = 25 - 4 = 21
+
+      // Act
+      var result = QuadraticEquationSolver.Solve(a, b, c);
+
+      // Assert
+      Assert.Equal(2, result.Length);
+      Assert.Equal(-0.20871215252208009, result[0]);
+      Assert.Equal(-4.7912878474779191, result[1]);
+    }
+  }
+}
---
+++ UnitTestingSamples/Sample5/DiscountModuleTests.cs
@@ -0,0 +1,112 @@
+﻿namespace UnitTestingSamples.Sample5;
+
+public class Goods
+{
+  public int Price { get; set; }
+  public string Name { get; set; }
+}
+
+public class DiscountModule
+{
+  public double Calculate(Goods[] goodsArray)
+  {
+    if (goodsArray.Length == 0)
+      return 0;
+    else if (goodsArray.Length == 1)
+    {
+      return goodsArray.Single().Price;
+    }
+    else if (goodsArray.Length == 2)
+    {
+      var twoItemsSorted = goodsArray.OrderBy(x => x.Price);
+      return twoItemsSorted.First().Price * 0.8 + twoItemsSorted.Last().Price;
+    }
+    var sorted = goodsArray.OrderBy(x => x.Price);
+    return sorted.First().Price * 0.7 + sorted.Skip(1).First().Price * 0.8 + sorted.Skip(2).Sum(x => x.Price);
+  }
+}
+
+public class DiscountModuleTests
+{
+  /*
+   * Test Cases:
+   * EmptyCart -> Price=0
+   * SingleGood(Price=100) -> Price=100
+   * TwoGoods(Price=100, Price=200) -> Price=280
+   * TwoGoods(Price=200, Price=100) -> Price=280
+   * ThreeGoods(Price=100, Price=200, Price=300) -> Price=70+160+300=530
+   * ThreeGoods(Price=300, Price=200, Price=100) -> Price=300+160+70=530
+   * ThreeGoods(Price=200, Price=300, Price=100) -> Price=300+160+70=530
+   * FourGoods(Price=100, Price=200, Price=300, Price=400) -> Price=70+160+300+400=930
+   */
+
+  [Fact]
+  public void EmptyCart_ShouldReturnZeroPrice()
+  {
+    // Arrange
+    var cart = new Goods[0];
+    var discountModule = new DiscountModule();
+
+    // Act
+    var price = discountModule.Calculate(cart);
+
+    // Assert
+    Assert.Equal(0, price);
+  }
+
+  [Fact]
+  public void SingleGood_ShouldReturnOriginalPrice()
+  {
+    // Arrange
+    var cart = new Goods[] { new Goods { Price = 100 } };
+    var discountModule = new DiscountModule();
+
+    // Act
+    var price = discountModule.Calculate(cart);
+
+    // Assert
+    Assert.Equal(100, price);
+  }
+
+  [Fact]
+  public void TwoGoods_InDifferentOrder_ShouldApplyDiscount()
+  {
+    // Arrange
+    var cart = new Goods[] { new Goods { Price = 200 }, new Goods { Price = 100 } };
+    var discountModule = new DiscountModule();
+
+    // Act
+    var price = discountModule.Calculate(cart);
+
+    // Assert
+    Assert.Equal(280, price);
+  }
+
+  [Fact]
+  public void ThreeGoods_InDifferentOrder_ShouldApplyDiscount()
+  {
+    // Arrange
+    var cart = new Goods[] { new Goods { Price = 200 }, new Goods { Price = 300 }, new Goods { Price = 100 } };
+    var discountModule = new DiscountModule();
+
+    // Act
+    var price = discountModule.Calculate(cart);
+
+    // Assert
+    Assert.Equal(530, price);
+  }
+
+  [Fact]
+  public void FourGoods_ShouldApplyDiscount()
+  {
+    // Arrange
+    var cart = new Goods[] { new Goods { Price = 100 }, new Goods { Price = 200 }, new Goods { Price = 300 }, new Goods { Price = 400 } };
+    var discountModule = new DiscountModule();
+
+    // Act
+    var price = discountModule.Calculate(cart);
+
+    // Assert
+    Assert.Equal(930, price);
+  }
+}
