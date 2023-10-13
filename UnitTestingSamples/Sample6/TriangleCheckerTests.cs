7f194bef5eefa347599f92e464f607b11a241bf1
Andrii Podkolzin <andrii.podkolzin@namecheap.com>
2023-10-13 12:08:22 +0300

    add unit tests

---
+++ UnitTestingSamples/Sample6/TriangleCheckerTests.cs
@@ -0,0 +1,52 @@
+﻿namespace UnitTestingSamples.Sample6;
+
+public class TriangleChecker
+{
+  public static bool Check(float a, float b, float c)
+  {
+    if (a <= 0 || b <= 0 || c <= 0)
+      return false;
+    else if (a >= b + c || b >= a + c || c >= a + b)
+      return false;
+    else
+      return true;
+  }
+}
+
+public class TriangleCheckerTests
+{
+  // -1, 3, 1 -> false
+  // 1, 0, 3 -> false
+  // 1, 2, -2 -> false
+  
+  // 2, 2, 2 -> true
+  
+  // 20, 2, 2 -> false
+  // 2, 20, 2 -> false
+  // 2, 2, 20 -> false
+  
+  [InlineData(-1, 3, 1)]
+  [InlineData(1, 0, 3)]
+  [InlineData(1, 2, -2)]
+  [Theory]
+  public void When_OneOfTheSidesIsNegative_Should_ReturnFalse(int a, int  b, int c)
+  {
+    // Act
+    var result = TriangleChecker.Check(a, b, c);
+    
+    // Assert
+    Assert.False(result);
+  }
+
+  [Fact]
+  public void When_AllSidesAreEqual_Should_ReturnTrue() => Assert.True(TriangleChecker.Check(2, 2, 2));
+
+  [InlineData(20, 2, 2)]
+  [InlineData(2, 20, 2)]
+  [InlineData(2, 2, 20)]
+  [Theory]
+  public void When_OneSideIsGreaterOrEqualThenTheSumOfTwoOthers_Should_Return_False(float a, float b, float c)
+  {
+    Assert.False(TriangleChecker.Check(a, b, c));
+  }
+}
---
+++ UnitTestingSamples/Sample7/ValidatorTests.cs
@@ -0,0 +1,59 @@
+﻿namespace UnitTestingSamples.Sample7;
+
+public enum ErrorCode {
+  InvalidName,
+  InvalidAge,
+  InvalidEmail
+}
+public record Error(string ErrorDescription, ErrorCode ErrorCode);
+public record ValidationResult(bool IsValid, IEnumerable<Error> Errors);
+
+class Validator
+{
+  public ValidationResult Validate(string name, int age, string email)
+  {
+    var errors = new List<Error>();
+    if (string.IsNullOrEmpty(name) || name.Length > 100)
+      errors.Add(new Error("Name can't be null, empty or longer than 100 chars", ErrorCode.InvalidName));
+    if (age is < 16 or > 200)
+      errors.Add(new Error("The age is invalid", ErrorCode.InvalidAge));
+    if (email.Length < 5 || !email.Contains("@") || !email.Contains("."))
+      errors.Add(new Error("Email should be longer than 5 chars, contain '.' and '@' chars", ErrorCode.InvalidEmail));
+    return new ValidationResult(errors.Count == 0, errors);
+  } 
+}
+
+
+public class ValidatorTests
+{
+  [InlineData("Andrii", 27, "apodkolzin@kse.org.ua", true, null, null)]
+  [InlineData("Andrii", 15, "apodkolzin@kse.org.ua", false, "The age is invalid", ErrorCode.InvalidAge)]
+  [InlineData("Andrii", 201, "apodkolzin@kse.org.ua", false, "The age is invalid", ErrorCode.InvalidAge)]
+  [InlineData("", 27, "apodkolzin@kse.org.ua", false, "Name can't be null, empty or longer than 100 chars", ErrorCode.InvalidName)]
+  [InlineData(null, 27, "apodkolzin@kse.org.ua", false, "Name can't be null, empty or longer than 100 chars", ErrorCode.InvalidName)]
+  [InlineData("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 27, "apodkolzin@kse.org.ua", false, "Name can't be null, empty or longer than 100 chars", ErrorCode.InvalidName)]
+  [InlineData("Andrii", 27, "apodkolzinkse.org.ua", false, "Email should be longer than 5 chars, contain '.' and '@' chars", ErrorCode.InvalidEmail)]
+  [InlineData("Andrii", 27, "apodkolzin@kseorgua", false, "Email should be longer than 5 chars, contain '.' and '@' chars", ErrorCode.InvalidEmail)]
+  [InlineData("Andrii", 27, "a@a.", false, "Email should be longer than 5 chars, contain '.' and '@' chars", ErrorCode.InvalidEmail)]
+  [Theory]
+  public void When_DataIsFilled_Result_ShouldBeAsExpected(string name, int age, string email, bool isValid, string? errorDescription, ErrorCode? errorCode)
+  {
+    // Arrange
+    var validator = new Validator();
+    
+    // Act
+    var result = validator.Validate(name, age, email);
+    
+    // Assert
+    Assert.Equal(isValid, result.IsValid);
+    if (!isValid)
+    {
+      Assert.Equal(errorCode, result.Errors.Single().ErrorCode);
+      Assert.Equal(errorDescription, result.Errors.Single().ErrorDescription);
+    }
+    else
+    {
+      Assert.Empty(result.Errors);
+    }
+  }
+}
